3112bcbbbdae327834bb4d72edccc21df810c3f1
Hampus Farner <hampusfarner@outlook.com>
2024-01-22 11:18:12 +0100

    creatediv now function instead of class

--- src/index.js
+++ src/index.js
@@ -6,21 +6,14 @@ element.innerText = 'blabla';
 document.body.appendChild(element);
 console.log('hello you');
 
-class Site {
-  createDiv() {
-    const div = document.createElement('div');
-    return div;
-  }
-
-  addClassToDiv(div, nameClass) {
-    div.classList.add(nameClass);
-  }
-
-  // perhaps getter and setter for class?
+function createDiv() {
+  const div = document.createElement('div');
+  return div;
 }
 
 class MainContent extends Site() {
   constructor() {
     this.title = 'dotCoffee';
   }
+  createMenuBar() {}
 }
